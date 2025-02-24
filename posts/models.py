f4cbef229aa25aac8124a644fea41308e39665b4
Sonia Islam <129697848+Developer-sonia@users.noreply.github.com>
2025-02-24 11:10:16 +0600

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.content[:20]
+        return f"Post by {self.user.username}"
