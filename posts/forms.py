af46dd161fa4cc030ce07e91db7d20d82dec1994
Sonia Islam <129697848+Developer-sonia@users.noreply.github.com>
2025-02-24 11:10:48 +0600

    Update forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,9 +1,7 @@
-# posts/forms.py
-
 from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image']  # Adjust this according to your Post model
+        fields = ['content', 'image']
